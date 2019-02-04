b6de9974c28ee8ad03df05cb60b4eeb3868e1497
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-04 19:49:58 +0900

    typeorm support on express server js

---
+++ app.ts
@@ -0,0 +1,53 @@
+'use strict';
+
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import express, { Request, Response, NextFunction } from 'express';
+import * as bodyParser from 'body-parser';
+import logger from 'morgan';
+import * as path from 'path';
+import cors from 'cors';
+import { indexRoutes } from './controllers';
+import http from 'http';
+
+createConnection().then(async connection => {
+  const app = express();
+
+  // middleware
+  app.use(cors({ credentials: true, origin: true }));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+
+  // error handlers
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const err: any = new Error('Not Found');
+    err.status = 404;
+
+    next(err);
+  });
+
+  app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.status || 500;
+
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(statusCode).send('Server Error');
+  });
+
+  // routes
+  app.use('/', indexRoutes);
+
+  // port
+  const port = process.env.PORT || '3000';
+
+  // server
+  const server = http.createServer(app);
+
+  server.listen(port);
+  server.on('error', (e: object) => console.error(e));
+  server.on('listen', (content) => console.log(content));
+});
--- entities/user.entity.ts
+++ entities/user.entity.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
