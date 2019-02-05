f41f6b882efafb29f23b7bdee31489337cb04669
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-05 22:46:57 +0900

    fix typeorm config and replace with build directory.

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,11 +1,16 @@
 'use strict';
 
 import { Router, Request, Response, NextFunction } from 'express';
+// import { getConnection } from 'typeorm';
+// import { User } from '../entities/user';
+
 
 const router: Router = Router();
 
 // Sample endpoint
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // const userRepo = getConnection().getRepository(User);
+  // console.log(userRepo);
   res.send('Hello world!');
 });
 
