55bfffc5c1c03f5f35aeacba1a9e0089007a698a
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-09 21:25:22 +0900

    add authentication middleware

---
+++ middlewares/auth.ts
@@ -0,0 +1,34 @@
+import jwt from 'express-jwt';
+
+// getting Token or Bearer
+const getTokenFromHeader = (req: any) => {
+  if (
+    (req.headers['authorization'] &&
+      req.headers.authorization.split(' ')[0] === 'Token') ||
+    (req.headers.authorization &&
+      req.headers.authorization.split(' ')[0] === 'Bearer')
+  ) {
+    return req.headers.authorization.split(' ')[1];
+  }
+
+  return null;
+};
+
+const secret = process.env.SECRET || 'secret';
+
+// authentication middleware
+const auth = {
+  required: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    getToken: getTokenFromHeader
+  }),
+  optional: jwt({
+    secret: secret,
+    userProperty: 'payload',
+    credentialsRequired: false,
+    getToken: getTokenFromHeader
+  })
+};
+
+export default auth;
