1d3270a832f6f779ae66db07890c65230c328410
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-09 20:39:10 +0900

    better user registration implementation

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
-// import { getConnection } from 'typeorm';
-import { validate } from 'class-validator';
+import { getConnection } from 'typeorm';
+// import { validate } from 'class-validator';
 
 import { User } from '../entities/user';
+import { validate } from 'class-validator';
 
 const router: Router = Router();
 
@@ -11,24 +12,25 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     // user data
     const userBody = req.body.user;
+    const userRepo = getConnection().getRepository(User);
 
     const passwordSaltHash = await User.getPasswordSaltHash(userBody.password);
 
-    const newUser = new User();
-    newUser.email = userBody.email;
-    newUser.password = userBody.password;
-    newUser.hash = passwordSaltHash.hash;
-    newUser.salt = passwordSaltHash.salt;
+    const newUser = userRepo.create(Object.assign({
+      ...userBody,
+      ...passwordSaltHash
+    }) as Object);
 
     const newUserErrors = await validate(newUser);
 
     if (newUserErrors.length > 0) {
       next(newUserErrors);
     } else {
-      const user = await newUser.save();
-
-      // return user email and token
-      res.json({ user: User.toAuthJSON(user) });
+      // typeorm catch error should be passed as an array for consistency
+      const user = await userRepo.save(newUser).catch((e: Error) => next([e]));
+      if (user) {
+        res.json({ user: User.toAuthJSON(user) });
+      }
     }
   }
 );
