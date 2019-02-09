dc039f86dccec291e8fa5c1e6f65e6496acbd77f
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-09 21:25:57 +0900

    passport authentication settings

--- services/passport.ts
+++ services/passport.ts
@@ -3,30 +3,36 @@ import passportLocal from 'passport-local';
 
 import { User } from '../entities/user';
 
-const LocalStrategy = passportLocal.Strategy;
+export const localStrategy = () => {
+  const LocalStrategy = passportLocal.Strategy;
 
-// login passport strategy
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'user[email]',
-      passwordField: 'user[password]'
-    },
-    (email: string, password: string, done: Function) => {
-      User.verifyEmail(email)
-        .then((currentUser: User) => {
-          if (!User.isValidPassword(currentUser, password)) {
-            return done(undefined, false, {
-              errors: { password: 'is invalid' }
-            });
-          }
-          return done(undefined, currentUser);
-        })
-        .catch(e =>
-          done(undefined, false, {
-            errors: { 'email or username': 'is invalid' }
+  // login passport strategy
+  return passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'user[email]',
+        passwordField: 'user[password]'
+      },
+      async (email: string, password: string, done: Function) => {
+        await User.verifyEmail(email)
+          .then(async (currentUser: User) => {
+            const isValidPassword = await User.isValidPassword(
+              currentUser,
+              password
+            );
+            if (!isValidPassword) {
+              return done(undefined, false, {
+                errors: { message: 'Invalid password.' }
+              });
+            }
+            return done(undefined, currentUser);
           })
-        );
-    }
-  )
-);
+          .catch(e =>
+            done(undefined, false, {
+              errors: { 'email or username': 'is invalid' }
+            })
+          );
+      }
+    )
+  );
+};
