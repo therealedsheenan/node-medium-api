a44f374015b0e2c657e5008fa66b3c16d82c2ef2
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-09 19:54:37 +0900

    user profile migration typeorm files

---
+++ migrations/1549708747445-UserProfile.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserProfile1549708747445 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
