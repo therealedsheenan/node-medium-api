7755a30b686efe9bf7e4045d30c9394a4ff9c347
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-11 20:51:32 +0900

    add clap controller and entities

---
+++ controllers/clap.ts
@@ -0,0 +1,49 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { getConnection } from 'typeorm';
+
+import { Clap } from '../entities/clap';
+
+const router: Router = Router();
+
+// Get claps count of a post
+router.get(
+  '/post/:postId/claps',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.postId;
+    const claps = await Clap.getClapsCount(postId).catch((e: Error) => next(e));
+
+    if (!claps) {
+      next();
+    }
+
+    res.json({ claps });
+  }
+);
+
+// Create clap to post
+router.post(
+  '/post/:postId/clap',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.postId;
+    const newClap = await Clap.create({ post: postId });
+    const clapRepo = getConnection().getRepository(Clap);
+
+    // save clap and return all claps the post
+    clapRepo
+      .save(newClap)
+      .then(async (clap: Clap) => {
+        const claps = await Clap.getClapsCount(postId).catch((e: Error) =>
+          next(e)
+        );
+
+        if (!claps) {
+          next();
+        }
+
+        res.json({ claps });
+      })
+      .catch((e: Error) => next(e));
+  }
+);
+
+export const clapsRoutes: Router = router;
--- entities/clap.ts
+++ entities/clap.ts
@@ -1,11 +1,17 @@
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, ManyToOne, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
 import { Post } from './post';
 
 @Entity()
-export class Clap {
+export class Clap extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @ManyToOne(type => Post, post => post.claps, { nullable: false })
   post: Post;
+
+  static getClapsCount (postId: number) {
+    return this.createQueryBuilder('clap')
+      .where('clap.post.id = :postId', { postId })
+      .getCount();
+  }
 }
