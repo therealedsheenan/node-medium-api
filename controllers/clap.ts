b8efcd8e17c60e5527ac3084f72977a57d2affd4
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2019-02-26 18:07:34 +0900

    refactoring comments controllers to be more verbose

--- controllers/clap.ts
+++ controllers/clap.ts
@@ -6,45 +6,43 @@ import { Clap } from '../entities/clap';
 const router: Router = Router();
 
 // Get claps count of a post
-router.get(
-  '/post/:postId/claps',
-  async (req: Request, res: Response, next: NextFunction) => {
-    const postId = req.params.postId;
-    const claps = await Clap.getClapsCount(postId).catch((e: Error) => next(e));
+const getClaps = async (req: Request, res: Response, next: NextFunction) => {
+  const postId = req.params.postId;
+  const claps = await Clap.getClapsCount(postId).catch((e: Error) => next(e));
 
-    if (!claps) {
-      // return 0 if claps there's no claps existing on a post
-      res.json({ claps: 0 });
-    } else {
-      res.json({ claps });
-    }
+  if (!claps) {
+    // return 0 if claps there's no claps existing on a post
+    res.json({ claps: 0 });
+  } else {
+    res.json({ claps });
   }
-);
+};
 
 // Create clap to post
-router.post(
-  '/post/:postId/clap',
-  async (req: Request, res: Response, next: NextFunction) => {
-    const postId = req.params.postId;
-    const newClap = await Clap.create({ post: postId });
-    const clapRepo = getConnection().getRepository(Clap);
+const createClap = async (req: Request, res: Response, next: NextFunction) => {
+  const postId = req.params.postId;
+  const newClap = await Clap.create({ post: postId });
+  const clapRepo = getConnection().getRepository(Clap);
 
-    // save clap and return all claps the post
-    clapRepo
-      .save(newClap)
-      .then(async (clap: Clap) => {
-        const claps = await Clap.getClapsCount(postId).catch((e: Error) =>
-          next(e)
-        );
+  // save clap and return all claps the post
+  clapRepo
+    .save(newClap)
+    .then(async (clap: Clap) => {
+      const claps = await Clap.getClapsCount(postId).catch((e: Error) =>
+        next(e)
+      );
 
-        if (!claps) {
-          next();
-        }
+      if (!claps) {
+        next();
+      }
 
-        res.json({ claps });
-      })
-      .catch((e: Error) => next(e));
-  }
-);
+      res.json({ claps });
+    })
+    .catch((e: Error) => next(e));
+};
+
+// Claps routes declarations
+router.get('/post/:postId/claps', getClaps);
+router.post('/post/:postId/clap', createClap);
 
 export const clapsRoutes: Router = router;
